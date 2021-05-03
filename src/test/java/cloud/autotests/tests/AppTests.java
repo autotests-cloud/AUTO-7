e11a8aac362de9d24844ee7cd1ef31d7dfd1f033
Администратор <maksimdemsky@gmail.com>
2021-05-04 02:01:58 +0300

    add tests search word on page

--- src/test/java/cloud/autotests/tests/AppTests.java
+++ src/test/java/cloud/autotests/tests/AppTests.java
@@ -1,15 +1,20 @@
 package cloud.autotests.tests;
 
+import com.codeborne.selenide.Condition;
 import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
 @Epic("any")
 @Feature("your")
 @Story("metadata")
-public class AppTests extends TestBase {
+public class AppTests  {
 
     @Test
     @Description("Soon to be implemented by QA.GURU engineers")
@@ -17,11 +22,11 @@ public class AppTests extends TestBase {
     void test() {
 
         step("open 'https://upyachka.ru/'", () -> {
-            // todo
+            open("https://upyachka.ru/");
         });
 
         step("Seen word 'СКЪРО' or 'БЕГN'", () -> {
-            // todo
+            $(".main").shouldHave(text("БЕГN"));
         });
 
     }
